30212813e55461a7fcdf154a5e18da2d22211789
EsztiS <eszti@eszti.local>
2015-08-16 18:27:49 -0400

    removing test_shiny

--- test_shiny/global.R
+++
@@ -1,6 +0,0 @@
-r_raw <- read.csv('data/receipts.csv', colClasses='character')
-r_nums <- as.data.frame(sapply(r_raw[13:72], FUN = function(x) as.numeric(gsub(",", "", x))))
-receipts <- cbind(r_raw[1:12], r_nums)
-
-r_years = grep('^X[1-2][0-9][0-9][0-9]',names(receipts))
-r_indices = grep('.name$|.Budget$',names(receipts))
